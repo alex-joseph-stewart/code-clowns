cf3c9bc66906017bf34bd3f1669c86c1045d786b
Alex <alexjstewart220@gmail.com>
2025-12-27 12:36:48 -0500

    refactor: resizes main logo

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="bg-primary min-h-screen flex justify-center align-top">
-      <Image src="/logo.svg" width="500" height="500" alt="code clowns logo" />
+    <main className="bg-primary min-h-screen flex justify-center">
+      <Image src="/logo.svg" width="600" height="600" alt="code clowns logo" />
     </main>
   );
 }
