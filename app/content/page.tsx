ee3722d962a37c437a2209adbc8fc2eb3b97e4fd
Alex <alexjstewart220@gmail.com>
2025-12-28 11:02:39 -0500

    refactor(contentOpts): centers contentOps

--- app/content/page.tsx
+++ app/content/page.tsx
@@ -1,9 +1,15 @@
+import ContentOpt from '../components/contentOpt';
+
 export default function ContentLanding() {
   return (
     <main>
       <h2 className="font-display text-center text-secondary text-4xl mt-10">
         What are you interested in checking out?
       </h2>
+      <div className="flex justify-center mt-10">
+        <ContentOpt img="/book.svg" optMsg="Written content" />
+        <ContentOpt img="/camera.svg" optMsg="Video content" />
+      </div>
     </main>
   );
 }
